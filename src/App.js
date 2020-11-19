cf861a7dfe1cd1ab3c44999c6624e3d8656f282d
Lazar Cosovic <lazarcosovic21@gmail.com>
2020-11-19 17:32:45 +0100

    added signIn form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register'
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
@@ -31,21 +33,23 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
-      box: {}
+      box: {},
+      route: 'signIn',
+      isSignedIn: false
     }
   }
 
   calculateFacePosition = (data) => {
     console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
     const faceDots = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image=document.getElementById('imageUrl');
-    const height=Number(image.height);
-    const width=Number(image.width);
+    const image = document.getElementById('imageUrl');
+    const height = Number(image.height);
+    const width = Number(image.width);
     return {
-      leftCol: faceDots.left_col*width,
-      topRow: faceDots.top_row*height,
-      rightCol: width - (faceDots.right_col*width),
-      bottomRow: height - (faceDots.bottom_row*height)
+      leftCol: faceDots.left_col * width,
+      topRow: faceDots.top_row * height,
+      rightCol: width - (faceDots.right_col * width),
+      bottomRow: height - (faceDots.bottom_row * height)
     }
   }
 
@@ -54,30 +58,50 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input })
     //FACE_DETECT_MODEL: '53e1df302c079b3db8a0a36033ed2d15'
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFace(this.calculateFacePosition(response)))
-    .catch(err => console.log('greska ',err))
+      .then(response => this.displayFace(this.calculateFacePosition(response)))
+      .catch(err => console.log('greska ', err))
   }
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value })
   }
 
-  displayFace=(box)=>{
-    this.setState({box:box});
+  displayFace = (box) => {
+    this.setState({ box: box });
+  }
+
+  onRouteChange = (route) => {
+    if(route==='signOut'){
+      this.setState({isSignedIn:false})
+    }else if(route==='home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({ route: route })
   }
 
   render() {
+    const {isSignedIn,route,box, imageUrl}=this.state;
     return (
       <div className="App">
         <Particles
           className='particles'
           params={{ particlesOptions }}
         />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange} />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {
+          route === 'home' ?
+            <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm onButtonSubmit={this.onButtonSubmit} onInputChange={this.onInputChange} />
+              <FaceRecognition box={box} imageUrl={imageUrl} />
+            </div>
+            :
+            (route==='signIn' ?
+            <SignIn onRouteChange={this.onRouteChange}/>
+            :
+            <Register onRouteChange={this.onRouteChange}/>)
+        }
       </div>
     );
   }
